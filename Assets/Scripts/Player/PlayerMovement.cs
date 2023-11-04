92645a077b8f16ef5be8f839a294b30a8037ff18
rusluck <russellymallari@gmail.com>
2023-11-04 14:38:14 -0400

    Renamed Folder

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,10 +1,9 @@
 using UnityEngine;
-using UnityEngine.XR;
 
-public class PlayerContext : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     //Depenceies
-    Rigidbody2D _rb;
+    Rigidbody2D _rigidbody;
 
     //Parameters for stuff
     [SerializeField] float _speed;
@@ -45,19 +44,21 @@ public class PlayerContext : MonoBehaviour
     void Awake()
     {
         //Setup dependencies
-        _rb = GetComponent<Rigidbody2D>();
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
+
+        UpdateCount();
+        if(_downDashCount > 0.5f)
+            return;
         //It'll just do all these
         HandleMovement();
         HandleJump();
         HandleGroundCheck();
         HandleDash();
         HandleDownDash();
-        
-        DelayDash();
     }
     
     //Walking movement
@@ -66,7 +67,7 @@ public class PlayerContext : MonoBehaviour
         _direction = Input.GetAxis("Horizontal");
 
 
-        _rb.position += Vector2.right * _direction * _speed * Time.deltaTime;
+        _rigidbody.position += Vector2.right * _direction * _speed * Time.deltaTime;
 
         if(Mathf.Abs(_direction) > 0.2f)
             transform.localScale = new Vector3(Mathf.Sign(_direction), 1f, 1f);
@@ -74,11 +75,13 @@ public class PlayerContext : MonoBehaviour
     //Jumping and stuff
     void HandleJump()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && _isGrounded)
-        {
-            _rb.AddForce(Vector2.up * Mathf.Sqrt(2 * 9.81f * _jumpHeight), ForceMode2D.Impulse);
+        if(!Input.GetKeyDown(KeyCode.Space))
+            return;
 
-        }
+        if(!_isGrounded)
+            return;
+
+        _rigidbody.AddForce(Vector2.up * Mathf.Sqrt(2 * 9.81f * _jumpHeight), ForceMode2D.Impulse);
     }
     //This will change whether grounded or not
     void HandleGroundCheck()
@@ -100,47 +103,62 @@ public class PlayerContext : MonoBehaviour
     //This is responsible for making player dash
     void HandleDash()
     {
-        if(Input.GetKeyDown(KeyCode.E) && _dashCount <= 0)
-        {
-            float mul = _isGrounded ? 1.5f : 1;
-            _rb.AddForce(Vector2.right * _dashDistance * Mathf.Sign(transform.localScale.x) * mul, ForceMode2D.Impulse);
-            //_rb.velocity = Vector2.right * _dashDistance * Mathf.Sign(transform.localScale.x) + Vector2.up * _rb.velocity.y;
+        if(!Input.GetKeyDown(KeyCode.E))
+            return;
 
-            _dashCount = 1;
-        }
+        if(_dashCount > 0)
+            return;
+        
+        _rigidbody.velocity = new Vector2(0, 1.5f);
+        float mul = _isGrounded ? 1.5f : 1;
+        _rigidbody.AddForce(Vector2.right * _dashDistance * Mathf.Sign(transform.localScale.x) * mul, ForceMode2D.Impulse);
+
+        _dashCount = 1;
+        
     }
 
     void HandleDownDash()
     {
-        if(Input.GetKeyDown(KeyCode.S) && _downDashCount <= 0)
-        {
-            _rb.velocity = Vector2.zero;
-            _rb.AddForce(Vector2.down * _downDashForce, ForceMode2D.Impulse);
-        }
+        if(!Input.GetKeyDown(KeyCode.S))
+            return;
+
+        if(_downDashCount > 0)
+            return;
+        
+        _rigidbody.velocity = Vector2.zero;
+        _rigidbody.AddForce(Vector2.down * _downDashForce, ForceMode2D.Impulse);
+
+        _downDashCount = 1;
+        
     }
 
-    void DelayDash()
+    void UpdateCount()
     {
-        //Code for normal dashing
+        UpdateDash();
+
+        UpdateDownDash();
+    }
+
+    void UpdateDash()
+    {
         if(_dashCount <= 0)
         {
             _dashCount = 0;
+            return;
         }
-        else
-        {
-            _dashCount -= Time.deltaTime / _dashDelay;
-        }
+        _dashCount -= Time.deltaTime / _dashDelay;
 
-        //Code for down dashing
+    }
 
+    void UpdateDownDash()
+    {
         if(_downDashCount <= 0)
         {
             _downDashCount = 0;
+            return;
         }
-        else
-        {
-            _downDashCount -= Time.deltaTime / _downDashDelay;
-        }
+        
+        _downDashCount -= Time.deltaTime / _downDashDelay;
+        
     }
-
 }
