33ed36561d0d9891755c78cedf7f17cf26c96147
rusluck <russellymallari@gmail.com>
2023-11-04 14:39:21 -0400

    Added basic enemy detection

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    //Dependencies
+    [SerializeField] GameObject _attackObject;
+    //Params
+    [SerializeField] float _damage;
+    [SerializeField] float _attackDuration;
+    [SerializeField] float _attackDelay;
+
+    //Buffers
+    float _attackCount;
+
+    void Update()
+    {
+
+        if(_attackCount > 0)
+        {
+            _attackCount = Mathf.Max(0, _attackCount - Time.deltaTime / _attackDelay);
+            return;
+        }
+        StartCoroutine(Attack());
+    }
+    IEnumerator Attack()
+    {
+        if(!Input.GetMouseButtonDown(0))
+            yield break;
+
+
+        _attackCount = 1;
+
+        List<RaycastHit2D> hits = new List<RaycastHit2D>();
+
+        ContactFilter2D cf = new ContactFilter2D();
+        cf.SetLayerMask(64);
+        cf.useTriggers = true;
+
+        _attackObject.GetComponent<Collider2D>().Cast(Vector2.down, cf, hits);
+
+
+        foreach(RaycastHit2D hit in hits)
+        {
+            Debug.Log(hit.transform.name);
+        }
+
+        _attackObject.GetComponent<SpriteRenderer>().enabled = true;
+        yield return new WaitForSeconds(_attackDuration);
+        _attackObject.GetComponent<SpriteRenderer>().enabled = false;
+    }
+}
